907d3103266bd33be780cde99e4056014bd28105
Andrew Dillard <a.dillard3@gmail.com>
2016-01-04 17:33:35 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
+
   root 'static_pages#home'
   get 'static_pages/home'
+  get 'signup' => 'users#new'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
